51ce9e0d3f3f578e96ec0291cf775a0723d6b585
Polkisss <createin2016lig@gmail.com>
2018-08-10 15:10:40 +0300

    Rewrite, a lot of fixes and improvements

--- swearfilter/filter.py
+++ swearfilter/filter.py
@@ -5,46 +5,73 @@ __author__ = 'Polkisss'
 __license__ = 'MIT'
 
 import re
-from inspect import getfile
 from os import environ
 from os import path
 
 from .settingswriter import Config
 
 
+class Message:
+    """
+    Represents Filter.msg as object
+
+    """
+    __slots__ = ['trip_amount', 'reaction', 'message', 'hasWords']
+
+    def __init__(self, trip_amount, reaction, message):
+        self.trip_amount = trip_amount
+        self.reaction = reaction
+        self.message = message
+        self.hasWords = trip_amount > 0
+
+    def __bool__(self):
+        return self.hasWords
+
+
 class Filter:
+    __slots__ = ['msg', 'reg_samples', '_tmp', '_blacklist', '_whitelist', '_other', 'priority_temp']
+
     def __init__(self, messsage: str):
         """
         Scanning message for words.
 
         Example:
 
-            from swearfilter import Filter
+            import swearfilter as sf
 
             if __name__ == '__main__':
-                f = Filter('Your text')
-                print(f.examine())
+                foo = sf.Filter('message').scan()
+                print('%s, %s' % (foo.hasWords, foo.message))
+
+        Output:
+            False, message
 
 
         :param messsage:
             Your text to scan.
         """
+
         self.msg = messsage.lower().replace('ё', 'е').replace('й', 'и')
         self.reg_samples = {}
 
-        location = path.dirname(getfile(Config))
+        location = path.dirname(__file__)
 
         def mkway(x: str):
             return path.join(location, x)
 
-        self._tmp = path.join(environ['TEMP'], '.~swfilter_temp~')
+        try:
+            self._tmp = path.join(environ['TEMP'], '.~swfilter_temp~')  # Windows
+        except KeyError:  # dividing by OS
+            self._tmp = '/tmp/.~swfilter_temp~'
 
         # Configuration variables
-        self.blacklist = Config(mkway('blacklist.txt')).get()['words']
-        self.whitelist = Config(mkway('whitelist.txt')).get()['whitelist']
-        self.other = Config(mkway('other.txt')).get()
+        self._blacklist = Config(mkway('blacklist.txt')).get()['words']
+        self._whitelist = Config(mkway('whitelist.txt')).get()['whitelist']
+        self._other = Config(mkway('other.txt')).get()
 
-    def examine(self):
+        # info variables
+
+    def scan(self):
         """
         Scan for swear words.
         :return:
@@ -55,18 +82,22 @@ class Filter:
         self._make_regex()  # caching
         self._dump_temp()
 
-        for word in self.whitelist:
+        for word in self._whitelist:
             self.msg = self.msg.replace(word, '')
 
-        for ex in self.other['special']:
-            self.msg = self.msg.replace(ex, '')
-
+        trips = 0
+        reaction = []
         for kw in self.reg_samples:
             match = re.findall(self.reg_samples[kw], self.msg)
             if match:
-                return True
+                reaction.append({  # some debug info
+                    'sample': self.reg_samples[kw],
+                    'matched': match[0],
+                    'on': kw,
+                })
+                trips += 1
 
-        return False
+        return Message(trips, reaction, self.msg, )
 
     def _make_regex(self):
         """
@@ -74,7 +105,7 @@ class Filter:
         :return:
         """
 
-        for word in self.blacklist:  # idk whats going on
+        for word in self._blacklist:  # idk whats going on
 
             if word in self.reg_samples:
                 continue
@@ -84,18 +115,21 @@ class Filter:
 
             for char in word:
                 tmp = ''
-                for alt_chars in self.other['symbols'][char]:
+                for alt_chars in self._other['symbols'][char]:
                     for alt_char in alt_chars:
                         tmp += alt_char
                 groups.append('[' + tmp + ']+')
 
             for i in groups:
                 sample.append(i)
-                sample.append(self.other['trash'])
+                sample.append(self._other['trash'])
 
             sample = ''.join(sample)
 
             self.reg_samples[word] = sample
+
+    def __str__(self):
+        return self.msg
 
     def _load_temp(self):
         try:
